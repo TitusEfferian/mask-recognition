1303c52bbc125c50b4191b2620627859f9f904ca
titusefferian <kiritosato@gmail.com>
2021-06-20 20:25:01 +0700

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,82 +1,75 @@
 import Head from 'next/head'
+import '@tensorflow/tfjs';
+
+import * as automl from '@tensorflow/tfjs-automl';
+import { useEffect, useRef, useState } from 'react';
+
+const frame = (video, model, canvas) => {
+  model.detect(video).then(prediction => {
+    const ctx = canvas.getContext('2d');
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    prediction.map(x => {
+      if (x.label === 'unmasked') {
+        ctx.strokeStyle = 'red';
+        ctx.lineWidth = 4;
+        ctx.strokeRect(x.box.left, x.box.top, x.box.width, x.box.height);
+        ctx.fillStyle = "white";
+        ctx.font = "30px Arial";
+        ctx.fillText(x.label, x.box.left, x.box.top);
+      }
+      else {
+        ctx.strokeStyle = 'green';
+        ctx.lineWidth = 4;
+        ctx.strokeRect(x.box.left, x.box.top, x.box.width, x.box.height);
+        ctx.fillStyle = "white";
+        ctx.font = "30px Arial";
+        ctx.fillText(x.label, x.box.left, x.box.top);
+      }
+    })
+    requestAnimationFrame(() => {
+      frame(video, model, canvas)
+    })
+  })
+}
+
 
 export default function Home() {
+  const videoRef = useRef();
+  const canvasRef = useRef();
+
+  useEffect(() => {
+    const initAutoML = async () => {
+      if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+        navigator.mediaDevices.getUserMedia({
+          audio: false,
+          video: {
+            facingMode: 'user',
+          }
+        }).then(stream => {
+          window.stream = stream;
+          videoRef.current.srcObject = stream;
+        })
+      }
+    }
+    initAutoML();
+  }, [])
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
-
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
+    <div className="w-full max-w-2xl mx-auto relative flex items-center justify-center">
+      <canvas ref={canvasRef} width="640" height="480" className="z-10">
+      </canvas>
+      <video
+        onLoadedMetadata={async () => {
+          const model = await automl.loadObjectDetection('model.json');
+          frame(videoRef.current, model, canvasRef.current);
+        }}
+        autoPlay
+        playsInline
+        muted
+        ref={videoRef}
+        width="640"
+        height="480"
+        className="absolute z-0"
+      />
     </div>
   )
 }
